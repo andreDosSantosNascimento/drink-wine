3215b3c33b2a1bd6fccc34373e058df64a384631
André dos Santos Nascimento <andredossantosnascimento1510@gmail.com>
2021-10-19 11:24:56 +0000

    feat: get client route

--- app/controllers/client_controller.py
+++ app/controllers/client_controller.py
@@ -1,11 +1,13 @@
 from app.models.client_model import Client
 from flask import current_app, jsonify, request
-from flask_jwt import JWT, jwt_required, current_identity
 
-data = request.json
+
+
 
 def create_client():
-    if not data['cpf'].isnumeric() or len(data['cpf'] != 11):
+    data = request.get_json()
+
+    if not data['cnpj'].isnumeric() or len(data['cnpj']) != 14:
         return '', 400
 
     user = Client(**data)
@@ -14,22 +16,35 @@ def create_client():
     session.add(user)
     session.commit()
 
-    return user, 201
+    return jsonify(user), 201
     ...
 
 def update_client():
+    data = request.get_json()
+
     user = Client(**data)
     session = current_app.db.session
     session.patch(user)
     session.commit()
-    return user, 200
+    return jsonify(user), 200
     ...
 
 def delete_client():
+    data = request.get_json()
+
     user = Client(**data)
     session = current_app.db.session
     session.delete(user)
     session.commit()
 
-    return user, 200
-    
+    return jsonify(user), 200
+    ...
+
+def get_client(id):
+    client = Client.query.get(id)
+
+    if not client:
+        return {"msg": "Not found!"}, 404
+        
+    return jsonify(client), 200
+    ...
