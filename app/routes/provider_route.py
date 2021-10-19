c0f7ffaccffffe0886efba8f3d2f07444ca0c1cd
Charles Pinheiro <charles_fagundes@hotmail.com>
2021-10-19 15:17:42 -0300

    feat/post-patch-delete-provider

--- app/routes/provider_route.py
+++ app/routes/provider_route.py
@@ -1,6 +1,8 @@
-from app.controllers.provider_controller import create
+from app.controllers.provider_controller import create_provider, delete_provider, update_provider
 from flask import Blueprint
 
 bp = Blueprint('bp_provider', __name__, url_prefix='/provider')
 
-bp.post('')(create)
+bp.post('')(create_provider)
+bp.patch('<int:id>')(update_provider)
+bp.delete('<int:id>')(delete_provider)
