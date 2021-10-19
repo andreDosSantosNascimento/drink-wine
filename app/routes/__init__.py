cf58edda58c882e3ec566031d3d03522d52fdd68
rodrigo_f <rodrigoloha@gmail.com>
2021-10-18 23:25:19 -0300

    feat: registred bp_client at routes.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,8 +1,10 @@
 from app.routes.provider_route import bp as provider_bp
 from app.routes.local_route import bp as local_bp
+from app.routes.client_route import bp as client_bp
 from flask import Flask
 
 
 def init_app(app: Flask):
     app.register_blueprint(provider_bp)
     app.register_blueprint(local_bp)
+    app.register_blueprint(client_bp)
