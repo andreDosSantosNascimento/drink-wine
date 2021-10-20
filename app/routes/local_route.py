54d5b37b180f573f26b84e6d65f4710bba58e32b
Guilherme Gonçalves <gcgoncalves13@gmail.com>
2021-10-20 10:20:03 -0300

    fix: get_local removed

--- app/routes/local_route.py
+++ app/routes/local_route.py
@@ -1,5 +1,5 @@
 from app.controllers.local_controller import (create_city, create_country,
-                                              create_state, get_local)
+                                              create_state)
 from flask import Blueprint
 
 bp = Blueprint('bp_local', __name__, url_prefix='/local')
@@ -7,4 +7,3 @@ bp = Blueprint('bp_local', __name__, url_prefix='/local')
 bp.post('/country')(create_country)
 bp.post('/state')(create_state)
 bp.post('/city')(create_city)
-bp.get('')(get_local)
