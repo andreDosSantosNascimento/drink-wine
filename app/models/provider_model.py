e134d66d328f8249637777515acb86ea0d1dfdc7
Andre Santos <andredossantosnascimento1510@gmail.com>
2021-10-20 22:11:09 -0300

    feat/ validate in model provider

--- app/models/provider_model.py
+++ app/models/provider_model.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
+from sqlalchemy.orm import validates
 
 from app.configs.database import db
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import backref, relationship
+
+from app.models.error_model import InvalidCnpjError, InvalidEmailError, WrongNumberFormatError, WrongTypeError
 
 
 @dataclass
@@ -17,3 +20,22 @@ class Provider(db.Model):
     country_id = Column(Integer, ForeignKey('countrys.id'), nullable=False)
     nif = Column(String(9), nullable=False, unique=True)
 
+    @validates("name","email", "phone", "nif")
+    def validate_types(self, key, value):
+        
+        if type(value) != str:
+            raise WrongTypeError(key, "string")
+
+        if key == "name":
+            value = value.title()
+        
+        if key == "nif" and not value.isnumeric():
+            raise InvalidCnpjError
+
+        if key == "phone" and not value.isnumeric():
+            raise WrongNumberFormatError
+        
+        if key == "email" and not "@" in value:
+            raise InvalidEmailError
+
+        return value
